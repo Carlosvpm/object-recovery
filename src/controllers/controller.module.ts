d0a1ec8adba9d351e24e3b1ec64eee82879904f2
Carlos Moraes <moraesvinicius_2002@hotmail.com>
2022-05-25 08:51:21 -0300

    ajustando modularização do projeto

--- src/controllers/controller.module.ts
+++ src/controllers/controller.module.ts
@@ -1,8 +1,6 @@
-import { SharedModule } from './../shared/shared.module';
-import { CarroService } from './../services/carro.service';
-import { TipoObjetoController } from './tipoobjeto.controller';
+import { CelularService } from 'src/services/celular.service';
+import { TipoObjetoService } from './../services/tipoobjeto.service';
 import { ModeloController } from './modelo.controller';
-import { MarcaController } from './marca.controller';
 import { DocumentoController } from './documento.controller';
 import { CelularController } from './celular.controller';
 import { CarroController } from './carro.controller';
@@ -11,22 +9,38 @@ https://docs.nestjs.com/modules
 */
 
 import { Module } from '@nestjs/common';
-import { ServiceModule } from 'src/services/service.module';
+import { MarcaController } from './marca.controller';
+import { TipoObjetoController } from './tipoobjeto.controller';
+import { CarroService } from 'src/services/carro.service';
+import { MarcaService } from 'src/services/marca.service';
+import { DocumentoService } from 'src/services/documento.service';
+import { ModeloService } from 'src/services/modelo.service';
+import { SharedModule } from 'src/shared/shared.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Celular } from 'src/models/celular.entity';
+import { Documento } from 'src/models/documento.entity';
+import { Carro } from 'src/models/carro.entity';
+import { Marca } from 'src/models/marca.entity';
+import { Modelo } from 'src/models/modelo.entity';
+import { TipoObjeto } from 'src/models/tipo-objeto.entity';
 
 @Module({
-  imports: [
-    ServiceModule,
-    SharedModule
-  ],
-
+  imports: [SharedModule, TypeOrmModule.forFeature([Celular, Carro, Documento, Marca, Modelo, TipoObjeto])],
   controllers: [
     CarroController,
     CelularController,
     DocumentoController,
     MarcaController,
-    ModeloController,
     TipoObjetoController,
+    ModeloController,
   ],
-  providers: [ServiceModule, CarroService],
+  providers: [
+    CarroService,
+    MarcaService,
+    DocumentoService,
+    TipoObjetoService,
+    CelularService,
+    ModeloService,
+  ],
 })
 export class ControllerModule {}
--- src/services/service.module.ts
+++ src/services/service.module.ts
@@ -1,31 +1,45 @@
-import { ControlerModule } from './../shared/controller/controler.module';
-import { TipoObjetoService } from './tipoobjeto.service';
-import { ModeloService } from 'src/services/modelo.service';
-import { MarcaService } from 'src/services/marca.service';
-import { DocumentoService } from 'src/services/documento.service';
-import { CelularService } from 'src/services/celular.service';
-import { CarroService } from 'src/services/carro.service';
-
+import { TipoObjeto } from './../models/tipo-objeto.entity';
+import { Modelo } from './../models/modelo.entity';
+import { Marca } from './../models/marca.entity';
+import { Documento } from './../models/documento.entity';
+import { Carro } from './../models/carro.entity';
+import { ModeloService } from './modelo.service';
 import { Module } from '@nestjs/common';
+import { CelularService } from './celular.service';
+import { CarroService } from './carro.service';
+import { DocumentoService } from './documento.service';
+import { MarcaService } from './marca.service';
+import { TipoObjetoService } from './tipoobjeto.service';
+import { DocumentoController } from 'src/controllers/documento.controller';
+import { CelularController } from 'src/controllers/celular.controller';
+import { CarroController } from 'src/controllers/carro.controller';
+import { ModeloController } from 'src/controllers/modelo.controller';
+import { TipoObjetoController } from 'src/controllers/tipoobjeto.controller';
+import { MarcaController } from 'src/controllers/marca.controller';
 import { SharedModule } from 'src/shared/shared.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Celular } from 'src/models/celular.entity';
 
 @Module({
-  imports: [SharedModule],
+  imports: [
+   SharedModule,
+   TypeOrmModule.forFeature([Celular, Carro, Documento, Marca, Modelo, TipoObjeto])
+  ],
+  controllers: [
+    CarroController,
+    CelularController,
+    DocumentoController,
+    MarcaController,
+    TipoObjetoController,
+    ModeloController,
+  ],
   providers: [
     CarroService,
-    CelularService,
-    DocumentoService,
     MarcaService,
-    ModeloService,
-    TipoObjetoService,
-  ],
-  exports: [
-    CarroService,
-    CelularService,
     DocumentoService,
-    MarcaService,
-    ModeloService,
     TipoObjetoService,
+    CelularService,
+    ModeloService,
   ],
 })
 export class ServiceModule {}
